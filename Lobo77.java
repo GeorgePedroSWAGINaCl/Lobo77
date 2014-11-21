c07ea23848540c18afc3de4c9621a878af4a2670
LaurineB <LaurineB@users.noreply.github.com>
2014-11-21 10:50:11 +0100

    Create Lobo77.java
    C'est le "controleur" du jeu, on va avoir ici toutes les méthodes qui vont être appelée par les interfaces pour que le jeu fonctionne

---
+++ Lobo77.java
@@ -0,0 +1,30 @@
+package Lobo77;
+
+
+public class Lobo77 {
+    Pioche pioche;
+    Defausse defausse;
+
+    /****************************
+     * Constructeur**************
+     ***************************/
+    public Lobo77()
+    {
+        pioche = new Pioche();
+        defausse = new Defausse();
+    }
+
+    /****************************
+     ******** Methodes************
+     ***************************/
+    public void defausserCarte()
+    {
+        this.main.
+    }
+    public boolean piocheEstVide()
+    {
+        return pioche.estVide();
+    }
+
+
+}
