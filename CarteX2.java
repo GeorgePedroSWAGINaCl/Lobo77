c15a64a6946cb4a7488ead8ace34fb795d7730ad
LaurineB <LaurineB@users.noreply.github.com>
2014-11-28 16:43:12 +0100

    Create CarteX2.java

---
+++ CarteX2.java
@@ -0,0 +1,14 @@
+package Lobo77;
+
+/**
+ * Created by Laurine on 28/11/2014.
+ */
+public class CarteX2 extends Carte {
+    public CarteX2() {
+        super(0);
+    }
+    /* règle : Si un joueur joue la carte «2x», le compte
+    reste inchangé et le joueur suivant doit poser
+    successivement 2 cartes, annoncer les 2
+    totaux successifs, puis piocher 2 fois.*/
+}
