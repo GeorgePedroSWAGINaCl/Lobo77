cf8b1cc88e52df0e0f6e55a0920404bfa9cec121
LaurineB <LaurineB@users.noreply.github.com>
2014-11-28 16:41:31 +0100

    Create CarteMoins10.java

---
+++ CarteMoins10.java
@@ -0,0 +1,17 @@
+package Lobo77;
+
+/**
+ * Created by Laurine on 28/11/2014.
+ */
+public class CarteMoins10 extends Carte {
+
+    public CarteMoins10()
+    {
+        super(-10);
+    }
+    /* règle : Si la carte «-10» est jouée, on soustrait 10
+    du dernier total annoncé et on l’annonce à
+    haute voix. On peut même éventuellement
+    obtenir momentanément un total négatif
+    inférieur à zéro. */
+}
