9e3b7587621f02d2a94f11f1054ba9dde450d16a
LaurineB <LaurineB@users.noreply.github.com>
2014-11-28 16:42:26 +0100

    Create CarteChgmtSens.java

---
+++ CarteChgmtSens.java
@@ -0,0 +1,15 @@
+package Lobo77;
+
+/**
+ * Created by Laurine on 28/11/2014.
+ */
+public class CarteChgmtSens extends Carte
+{
+    public CarteChgmtSens() {
+        super(0);
+    }
+    /* règle : Quand une carte «changement de sens»
+    est jouée, le dernier total est répété et le
+    sens de rotation inversé jusqu’au prochain
+    changement de sens.*/
+}
