a3f9c84eb6491f378ec5b451443f3a3b10938142
Jonathan Ferreira <AlphaJon@users.noreply.github.com>
2014-12-12 16:21:09 +0100

    Version 2 client /!\
    Attention: ne pas utiliser en cet état, non terminé

--- LoboClient.java
+++ LoboClient.java
@@ -6,28 +6,44 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-/**
- * Created by jon on 08/12/14.
- */
+
+
 public class LoboClient {
 	Socket socket;
-	BufferedReader in;
+	//BufferedReader in;
 	PrintWriter out;
+	LoboReceveur in;
+	Scanner scanner;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {// A enlever lors de l'intégration
 		new LoboClient();
 	}
+
 	public LoboClient() {
 		try {
-			socket = new Socket(InetAddress.getLocalHost(), 9001);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			socket = new Socket(InetAddress.getLocalHost(), 9001); //Changer si serveur <> client
+			//socket.setSoTimeout(1000);
+			//in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			in = new LoboReceveur(socket);
+			//Scanner inputscan = new Scanner(socket.getInputStream());
 			out = new PrintWriter(socket.getOutputStream(),true);
-			System.out.println(recevoir()); //Message de connexion
-			String s = "";
-			Scanner scanner = new Scanner(System.in);
+			System.out.println(recevoir()); //Message de connexion "connexion etablie"
+			String s;
+			String r;
+			scanner = new Scanner(System.in);
+			s = scanner.nextLine();
+			if (s.equals("")) s = "Anonymous";
+			out.println(s); //Envoi du nom
+			s = ""; //Au cas ou le nom est "Quitter"
+			//r = "Jouer";
 			while (!s.equalsIgnoreCase("Quitter")){
+				//while (!r.equalsIgnoreCase("Jouer")){
 				s = scanner.nextLine();
 				envoyer(s);
+
+				//}
+				/*s = scanner.nextLine();
+				envoyer(s);*/
 			}
 			quitter();
 		} catch (UnknownHostException e) {
@@ -38,26 +54,39 @@ public class LoboClient {
 	}
 
 	public void quitter(){
-		out.println("Quitter");
 		try {
-			socket.close();
+			if (!socket.isClosed()) {
+				out.println("Quitter");
+				in.exit();
+				socket.close();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void envoyer(String s){
-		if(s.equalsIgnoreCase("Quitter"))//Non sensible a la casse
+		//out.println(s);
+		if (s.equalsIgnoreCase("Quitter"))//Non sensible a la casse
 			quitter();
-		else
+		if (s.equalsIgnoreCase("Commencer") || s.equalsIgnoreCase("Recommencer")){
+			out.println("Commencer");
+			//s = "Commencer";
+			//recevoirCarte(5);
+		}
+		if (s.equalsIgnoreCase("Jouer")){
 			out.println(s);
+			jouerCarte(scanner.nextInt());
+		}
+		//out.println(s);
+
 	}
 
 	public String recevoir() throws IOException {
 		return in.readLine();
 	}
 
-	public Carte recevoirCarte(){
+	public Carte recevoirCarte(){ //TODO: A traiter
 		try {
 			return new Carte(in.read());
 		} catch (IOException e) {
@@ -66,18 +95,85 @@ public class LoboClient {
 		}
 	}
 
-	public void recevoirCarte(int n){
-		while (n>0){
-			recevoirCarte(); //TODO: A traiter
-			n--;
-		}
-	}
-	public void jouerCarte(String s){
+
+	public void jouerCarte(String s){ //Utiliser l'autre méthode au possible
 		envoyer("Jouer");
 		envoyer(s);
+		recevoirCarte();
 	}
 
 	public void jouerCarte(int i){
 		jouerCarte("" + i);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+class LoboReceveur extends Thread{
+	Socket socket;
+	BufferedReader in;
+	String lastmessage;
+	boolean exec;
+	int[] cartes;
+
+	public LoboReceveur(Socket s){
+		socket = s;
+		exec = true;
+		try {
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		start();
+	}
+
+	public void run(){
+		while (exec){
+			try {
+				lastmessage = in.readLine();
+				if (in != null) System.out.println(lastmessage);
+				if (lastmessage == "Jouer"){
+					wait();
+				}
+				if (lastmessage == "Commencer"){
+					cartes = new int[5];
+					for (int i = 0; i < 5; i++) {
+						cartes[i] = read();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public String readLine(){
+		return lastmessage;
+	}
+
+	public int read() throws IOException {
+		return in.read();
+	}
+
+	public void exit(){
+		try {
+			in.close();
+			exec = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int[] getCartes(){
+		return cartes;
 	}
 }
